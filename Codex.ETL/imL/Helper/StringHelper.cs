cf82019165d1f497e12ad102352bb4df883cd4e8
Ricardo Llamocca <r.llamocca@outlook.com>
2021-02-17 16:06:53 -0300

    ...

--- Codex.ETL/imL/Helper/StringHelper.cs
+++ Codex.ETL/imL/Helper/StringHelper.cs
@@ -44,25 +44,5 @@ namespace Codex.ETL.Helper
                 }
             }
         }
-
-        //        //ESTO NO DEBERIA ESTAR AQUI
-        //        private static Encoding Codificacion(String _a)
-        //        {
-        //            Byte[] _b = new Byte[5];
-        //            using (FileStream _fs = new FileStream(_a, FileMode.Open, FileAccess.Read))
-        //                _fs.Read(_b, 0, 5);
-
-        //            if (_b[0] == 0xFF && _b[1] == 0xFE && _b[2] == 0x00 && _b[3] == 0x00) return Encoding.UTF32;
-        //            if (_b[0] == 0x00 && _b[1] == 0x00 && _b[2] == 0xFE && _b[3] == 0xFF) return new UTF32Encoding(true, true);
-
-        //            if (_b[0] == 0xFF && _b[1] == 0xFE) return Encoding.Unicode;
-        //            if (_b[0] == 0xFE && _b[1] == 0xFF) return Encoding.BigEndianUnicode;
-
-        //            if (_b[0] == 0xEF && _b[1] == 0xBB && _b[2] == 0xBF) return Encoding.UTF8;
-
-        //            if (_b[0] == 0x2B && _b[1] == 0x2F && _b[2] == 0x76 && _b[3] == 0x38 && _b[4] == 0x2D) return Encoding.UTF7;
-
-        //            return Encoding.ASCII;
-        //        }
     }
 }
